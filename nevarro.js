d225bcf741328cd40d323658ebf69c5e058533ae
Mark Szulc <mszulc@adobe.com>
2021-08-01 14:42:06 +1000

    Only display if coral-Shell-homeAnchor is present

--- nevarro.js
+++ nevarro.js
@@ -1,2 +1,4 @@
 var homeAnchor = document.getElementsByClassName("_coral-Shell-homeAnchor");
-homeAnchor[0].innerHTML = homeAnchor[0].innerHTML + "<coral-shell-homeanchor-label>Adobe Experience Manager - Nevarro P31363</coral-shell-homeanchor-label>"
+if (homeAnchor.length > 0) {
+    homeAnchor[0].innerHTML = homeAnchor[0].innerHTML + "<coral-shell-homeanchor-label>Adobe Experience Manager - Nevarro P31363</coral-shell-homeanchor-label>"
+}
