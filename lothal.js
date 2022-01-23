bf8162cc8122a27457fa57458cc323b8b849cd6b
Mark Szulc <mszulc@adobe.com>
2022-01-24 10:28:17 +1100

    updated environments

---
+++ lothal.js
@@ -0,0 +1,4 @@
+var homeAnchor = document.getElementsByClassName("_coral-Shell-homeAnchor");
+if (homeAnchor.length > 0) {
+    homeAnchor[0].innerHTML = homeAnchor[0].innerHTML + "<coral-shell-homeanchor-label>Adobe Experience Manager - Lothal P55117</coral-shell-homeanchor-label>"
+}
