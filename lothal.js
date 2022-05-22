e1734b4faab153724d253687ec949bec34f413c4
Mark Szulc <mszulc@adobe.com>
2022-05-22 18:24:14 +1000

    Update Page Title

--- lothal.js
+++ lothal.js
@@ -1,6 +1,7 @@
 var homeAnchor = document.getElementsByClassName("spectrum-Shell-logoLabel");
 if (homeAnchor.length > 0) {
     setTimeout(function() { 
-        homeAnchor[0].innerHTML = homeAnchor[0].innerHTML + "<coral-shell-homeanchor-label> - Lothal DEV P55117</coral-shell-homeanchor-label>"
+        homeAnchor[0].innerHTML = homeAnchor[0].innerHTML + "<coral-shell-homeanchor-label> - Lothal DEV P55117</coral-shell-homeanchor-label>";
+        document.querySelector('title').textContent += "- LOTHAL";
     }, 3500);
 }
--- scarif.js
+++ scarif.js
@@ -1,6 +1,7 @@
 var homeAnchor = document.getElementsByClassName("spectrum-Shell-logoLabel");
 if (homeAnchor.length > 0) {
     setTimeout(function() { 
-        homeAnchor[0].innerHTML = homeAnchor[0].innerHTML + "<coral-shell-homeanchor-label> - Scarif DEV P47527</coral-shell-homeanchor-label>"
+        homeAnchor[0].innerHTML = homeAnchor[0].innerHTML + "<coral-shell-homeanchor-label> - Scarif DEV P47527</coral-shell-homeanchor-label>";
+        document.querySelector('title').textContent += "- SCARIF";
     }, 3500);
 }
